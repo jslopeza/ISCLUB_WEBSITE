3d4179d17b1fb35c0df8698d99fff2e179e7f0df
jslopeza <jasonlopeza95@gmail.com>
2014-05-16 19:52:50 -0400

    Changed the password

--- PHP/connect.php
+++ PHP/connect.php
@@ -3,7 +3,7 @@
 try
 {
 	// Connect to DB
-	$dbh = new PDO('mysql:host=localhost;dbname=isclub;charset=utf8','root','njitis1234');
+	$dbh = new PDO('mysql:host=localhost;dbname=isclub;charset=utf8','root','njit1234');
 	echo $dbh;
 }
 catch(PDOException $e)
