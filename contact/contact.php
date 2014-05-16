eb4d906bb9335ceaea316987dd589d134f01f7db
jslopeza <jasonlopeza95@gmail.com>
2014-05-16 18:34:25 -0400

    Contact form with mail

--- contact/contact.php
+++ contact/contact.php
@@ -1,6 +1,43 @@
 <?php require('../PHP/connect.php');
 	error_reporting(E_ALL);
 	
+	// Getting the data
 	$name = $_POST['name'];
-	echo $name;
+	$company = $_POST['company'];
+	$email = $_POST['email'];
+	$comments = $_POST['comments'];
+
+	// Insert 
+	$sth = $dbh->prepare("INSERT INTO contact(name, company, email, comments)
+						  VALUES(:name, :company, :email, :comments)");
+
+
+	// Bind Values
+	$sth->bindParam(':name',$name);
+	$sth->bindParam(':company',$company);
+	$sth->bindParam(':emai',$email);
+	$sth->bindParam(':comments',$comments);
+
+	// Execute
+	$sth->execute();
+
+	// Mail
+	$to = "binoypatel14@gmail.com";
+	$from = "admin@njitisclub.org";
+	$subject = "Someone Contacted";
+	$replyto = "njitisclub@gmail.com"
+
+	$headers = "From: " . $from . "\r\n";
+	$headers = "Reply-To ".$replyto."\r\n";
+	$headers = "MIME-Version: 1.0\r\n";
+	$headers = "Content-Type: text/html; charset=ISO-8850-1\r\n";
+
+	$message = "<html><body>";
+	$message = "<h1>Someone Contacted</h1>";
+	$message = "<p>Name: ".$name."<br>Company: ".$company."<br>Email".$email."<br>Comments: ".$comments."</p>";
+
+	mail($to, $subject, $message, $headers);
+
+	// Redirect
+	header('Location: ../index.html');
 ?>
